89e139fcbcf6c4571703b7bcd9e73f54160ef431
QuantumDev-CERN <adityabnw07@gmail.com>
2026-04-06 02:56:47 +0530

    Threads and Communities are Ready

--- api/src/index.js
+++ api/src/index.js
@@ -3,8 +3,8 @@ import dotenv from'dotenv';
 
 import authRoutes from './routes/auth.js';
 //import userRoutes from './routes/users.js';
-//import communityRoutes from './routes/communities.js';
-//import threadRoutes from './routes/threads.js';
+import communityRoutes from './routes/communities.js';
+import threadRoutes from './routes/threads.js';
 //import photoRoutes from './routes/photos.js';
 //import searchRoutes from './routes/search.js';
 
@@ -17,10 +17,11 @@ app.use(express.json()); //Parse JSON bodies
 //Routes
 app.use('/api/auth', authRoutes);
 //app.use('/api/users', userRoutes);
-//app.use('/api/communities', communityRoutes);
-//app.use('/api/threads', threadRoutes);
+app.use('/api/communities', communityRoutes);
+app.use('/api/threads', threadRoutes);
 //app.use('/api/photos', photoRoutes);
 //app.use('/api/search', searchRoutes);
+
 
 //Global error handler
 app.use((err, req, res, next) => {
