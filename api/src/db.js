1d5bf8ebcab6c016dd2a05588a6430aa3f87b669
QuantumDev-CERN <adityabnw07@gmail.com>
2026-04-05 05:42:37 +0530

    Set up Pool of connection , system-design choice for performance

--- api/src/db.js
+++ api/src/db.js
@@ -1,2 +1,11 @@
-// PostgreSQL pool placeholder.
-export {};
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {Pool} = pg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+export default pool;
