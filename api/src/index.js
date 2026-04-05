e54a508450c9a3751ed560f0aced2f286000819e
QuantumDev-CERN <adityabnw07@gmail.com>
2026-04-05 08:46:16 +0530

    Fixed import , needs .js too for file when importing

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import dotenv from'dotenv';
 
-import authRoutes from './routes/auth';
-import userRoutes from './routes/users';
-import communityRoutes from './routes/communities';
-import threadRoutes from './routes/threads';
-import photoRoutes from './routes/photos';
-import searchRoutes from './routes/search';
+import authRoutes from './routes/auth.js';
+//import userRoutes from './routes/users.js';
+//import communityRoutes from './routes/communities.js';
+//import threadRoutes from './routes/threads.js';
+//import photoRoutes from './routes/photos.js';
+//import searchRoutes from './routes/search.js';
 
 dotenv.config();
 
@@ -16,11 +16,11 @@ app.use(express.json()); //Parse JSON bodies
 
 //Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/communities', communityRoutes);
-app.use('/api/threads', threadRoutes);
-app.use('/api/photos', photoRoutes);
-app.use('/api/search', searchRoutes);
+//app.use('/api/users', userRoutes);
+//app.use('/api/communities', communityRoutes);
+//app.use('/api/threads', threadRoutes);
+//app.use('/api/photos', photoRoutes);
+//app.use('/api/search', searchRoutes);
 
 //Global error handler
 app.use((err, req, res, next) => {
