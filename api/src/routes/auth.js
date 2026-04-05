0f8e0a3a2c20c84ba894742a9ee2a40d7db5851e
QuantumDev-CERN <adityabnw07@gmail.com>
2026-04-05 07:43:53 +0530

    Login Endpoint Created

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -50,3 +50,43 @@ router.post('/register', async (req, res, next) => {
     }
 
 });
+
+// POST /api/auth/login
+router.post('/login', async(req, res, next) => {
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({error: 'email and password are required'});
+        }
+
+        const result = await pool.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({error: 'Invalid credentials'});
+        }
+
+        const user = result.rows[0];
+        const valid = await bcrypt.compare(password, user.password_hash);
+
+        if (!valid) {
+            return res.status(401).json({error: 'Invalid Credentials'});
+        }
+
+        const token = jwt.sign(
+            {id: user.id, username: user.username, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '7d'}
+        );
+
+        // Dont send Password_hash back
+        const {password_hash, ...safeUser} = user;
+
+        res.json({token, user:safeUser});
+    } catch (err) {
+        next(err);
+    }
+});
