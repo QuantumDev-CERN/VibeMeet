2a17caa9dd5c9c5863add0ff7d2e53428a1e664d
QuantumDev-CERN <adityabnw07@gmail.com>
2026-04-05 19:49:51 +0530

    Can Search up Single Community by slug

--- api/src/routes/communities.js
+++ api/src/routes/communities.js
@@ -75,3 +75,24 @@ router.get('/', authenticate, async (req, res, next) => {
         next(err)
     }
 });
+
+router.get('/:slug', async (req, res, next) => {
+    try{
+        const { slug } = req.params;
+
+        const result = await pool.query(`
+            SELECT c.id, c.name, c.description, c.banner_url, c.created_at,
+            (SELECT COUNT (*) FROM community_members cm WHERE cm.community_id = c.id) AS member_count
+            FROM communities c WHERE c.slug = $1
+            `,[slug]);
+        
+        if (results.rows.length === 0) {
+            return res.status(404).json({error: 'Community not found'});
+        }
+
+        res.json({ community: result.rows[0] });
+
+    } catch (err) {
+        next(err);
+    }
+});
